fb14bd7fb9be71f056050ec376116af632cbf75c
superstarlq <superstarliqun@gmail.com>
2021-12-16 16:59:31 +0800

    Changes

--- src/main/java/com/whkj/project/controller/UserController.java
+++ src/main/java/com/whkj/project/controller/UserController.java
@@ -3,11 +3,16 @@ package com.whkj.project.controller;
 import com.github.pagehelper.PageInfo;
 import com.whkj.project.entity.User;
 import com.whkj.project.service.UserService;
+import com.whkj.project.utils.ExcelUtil;
 import com.whkj.project.utils.RestResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping(value = "/User/Api")
@@ -26,6 +31,15 @@ public class UserController {
     public RestResult findUserPage(User user, PageInfo pageInfo){
         return RestResult.ok(userService.findUserPage(user,pageInfo));
 
+    }
+
+    @PostMapping(value = "Excelexport")
+    public RestResult Excelexport(){
+        User user = userService.selectByPrimaryKey("1", "1");
+
+
+        ExcelUtil.importDataFromExcel(user,"","你好文件");
+        return RestResult.ok();
     }
 
 }
